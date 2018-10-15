bb2d24f03acc2d5a061ad9761f0947e6fb3c377a
Mohameed imam <imam@VYSHNAVI002s-MacBook-Air.local>
2018-10-15 18:15:59 +0530

    implement cpwd and add design

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,23 +63,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
 
-#   config.action_mailer.default charset: 'utf-8'
-#   config.action_mailer.delivery_method = :smtp
-# #  config.action_mailer.perform_deliveries = true
 
-#   config.action_mailer.smtp_settings = {
-
-#     address: "smtp.gmail.com",
-#     port: 587,
-#     domain: "gmail.com"
-#     authentication: "plain",
-#     enable_starttls_auto: true;
-
-#     user_name: ENV["imam.sykamcs@gmail.com"],
-#     password: ENV["Imamscs9$1#1"]
-
-  
-# }
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.default charset: 'utf-8'
