57b68caef7623dca13bcd82f2d43458aed4ecd13
Mohameed imam <imam@VYSHNAVI002s-MacBook-Air.local>
2018-10-16 18:40:38 +0530

    added confirmation and trackable module

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
         :recoverable, :rememberable,:trackable, :validatable
+
+protected
+def confirmation_required?
+  true
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'pages/destroy'
   get 'pages/new'
   get  'pages/about'
-  devise_for :users
+   devise_for :users,controllers: { confirmations: 'confirmations' } 
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
