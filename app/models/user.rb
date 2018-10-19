4a5ee79d92daef1cda10fa96f9ab9363e923bc38
Mohameed imam <imam@VYSHNAVI002s-MacBook-Air.local>
2018-10-19 17:34:10 +0530

    modified changepassword

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,27 @@
 class User < ApplicationRecord
-  # after_update :send_password_change_email, if: :needs_password_change_email?
+
+
+ after_update :send_password_change_email, if: :needs_password_change_email?
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
         :recoverable, :rememberable,:trackable, :timeoutable,:validatable
+# validates_confirmation_of :password
+#  validates_presence_of     :password, if: :password_required?
+#           validates_confirmation_of :password, if: :password_required?
+#           validates_length_of       :password, within: password_length, allow_blank: true
+# validates_presence_of   :email, if: :email_required?
 
 
 
+# def password_required?
+#  !persisted? || !password.nil? || !password_confirmation.nil?
+# end
+
+# def email_required?
+#    true
+# end
 
 protected
   def confirmation_required?
@@ -20,18 +35,67 @@ WelcomeMailer.welcome_send(self).deliver
 # redirect_to root_path, alert:"Thank you for the registration"
 end
 
-# private
-# def needs_password_change_email?
-#     encrypted_password_changed? && persisted?
-#   end
+private
+def needs_password_change_email?
+    encrypted_password_changed? && persisted?
+  end
    
-#   def send_password_change_email
-#     UserMailer.password_changed(id).deliver
+  def send_password_change_email
+    UserMailer.password_changed(id).deliver
+ end
+
+
+
+# after_update :send_password_reset_email
+# def send_password_reset_email
+# 	UserMailer.password_changed(id).deliver
 #   end
 
-after_update :send_password_reset_email
-def send_password_reset_email
-    UserMailer.password_changed(id).deliver_now
-  end
+#  after_update :send_password_reset_email
+# def send_password_reset_email
+# user = User.find_by_id(id)
+# unless user.blank?
+      # if user.update_attributes(user.password.to_h)//update_with_password
+#         UserMailer.password_changed(user).deliver
+#       else
+#         render :action => 'edit'
+#       end
+#  else
+#       render :action => 'edit'
+#   end
+# end
+
+
+
+# Email changed
+# after_create :account_created
+
+# # called after the account is first created
+# def account_created
+
+#   # check if this activiy has already been created
+#   if !self.activities.where(:kind => "created_account").blank?
+#     puts "WARNING: user ##{self.id} already has a created account activity!"
+#     return
+#   end
+
+#   # update points
+#   self.points += 50
+#   self.save
+
+#   # create activity
+#   act = self.activities.new
+#   act.kind = "created_account"
+#   act.created_at = self.created_at
+#   act.save
+
+# end
+
+
+# def confirm!
+#   super
+#   account_created
+# end
+
 
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -124,6 +124,8 @@ Devise.setup do |config|
   # Send a notification email when the user's password is changed.
   # config.send_password_change_notification = false
 
+  config.send_password_change_notification = true
+
   # ==> Configuration for :confirmable
   # A period that the user is allowed to access the website even without
   # confirming their account. For instance, if set to 2.days, the user will be
@@ -146,7 +148,7 @@ Devise.setup do |config|
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
   # db field (see migrations). Until confirmed, new email is stored in
   # unconfirmed_email column, and copied to email column on successful confirmation.
-  config.reconfirmable = false
+  config.reconfirmable = true
 
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [:email]
@@ -172,12 +174,12 @@ Devise.setup do |config|
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
   # to give user feedback and not to assert the e-mail validity.
-  config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
-
+  # config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
+config.email_regexp = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
-  config.timeout_in = 1.minutes
+  config.timeout_in = 30.minutes
 
   # ==> Configuration for :lockable
   # Defines which strategy will be used to lock an account.
